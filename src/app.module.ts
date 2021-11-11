603dcc41bf83169e5ee30f03278587bdfa417ecd
MaksGovor <maksgovruha@gmail.com>
2021-11-12 00:01:43 +0200

    Fix non-working controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './controller/app.controller';
 import { AppService } from './service/app.service';
+import { CatsController } from './controller/cats.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, CatsController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(`Application is running on: ${await app.getUrl()}`);
   await app.listen(3000);
+  
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
