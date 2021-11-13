a41e9d6c426b3277c956355273225aed2a68d523
dmytro <tedi4t@gmail.com>
2021-11-13 16:38:37 +0200

    healthy endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './controller/app.controller';
+import { SystemController } from './controller/system.comtroller';
 import { AppService } from './service/app.service';
 import { CatsController } from './controller/cats.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, CatsController],
+  controllers: [AppController, CatsController, SystemController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/controller/system.comtroller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, HttpCode, HttpStatus, Res } from '@nestjs/common';
+import { Response } from 'express';
+
+@Controller('system')
+export class SystemController {
+  constructor() {}
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  healthy(
+    @Res() res: Response,
+  ): void {
+    res.status(HttpStatus.OK).send();
+  }
+}
