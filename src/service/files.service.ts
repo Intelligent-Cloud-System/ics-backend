ff2275d619e9586ece585628ea3bd60a0ac5af10
MaksGovor <maksgovruha@gmail.com>
2021-11-25 18:56:08 +0200

    Add service for dir creating

--- src/service/files.service.ts
+++ src/service/files.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import { writeFile } from 'fs/promises';
+import { writeFile, mkdir } from 'fs/promises';
 import { join } from 'path';
+
+const outputFolder = join(process.cwd(), 'fs-ics');
 
 @Injectable()
 export class FilesService {
-  writeFileUser(fileName: string, buffer: Buffer): Promise<any> {
-    const outputPath = join(process.cwd(), 'fs-ics', fileName);
+  writeFileUser(fileName: string, buffer: Buffer): Promise<void> {
+    const outputPath = join(outputFolder, fileName);
     return writeFile(outputPath, buffer);
+  }
+
+  registerDirectory(dirName: string): Promise<void> {
+    const dirPath = join(outputFolder, dirName);
+    return mkdir(dirPath);
   }
 }
