9ff967a9543d32276b4d364c70f35cce03b0323f
MaksGovor <maksgovruha@gmail.com>
2021-11-29 23:04:39 +0200

    Refactor file service

--- src/service/files.service.ts
+++ src/service/files.service.ts
@@ -3,17 +3,20 @@ import { writeFile, mkdir } from 'fs/promises';
 import { existsSync } from 'fs';
 import { join } from 'path';
 
-const outputFolder = join(process.cwd(), 'fs-ics');
+const STORAGE_PATH = join(process.cwd(), './storage/');
 
 @Injectable()
 export class FilesService {
-  writeFileUser(fileName: string, buffer: Buffer): Promise<void> {
-    const outputPath = join(outputFolder, fileName);
+  async writeFileUser(fileName: string, buffer: Buffer): Promise<void> {
+    const outputPath = join(STORAGE_PATH, fileName);
+    
+    
+
     return writeFile(outputPath, buffer);
   }
 
   registerDirectory(dirName: string): Promise<void> {
-    const dirPath = join(outputFolder, dirName);
+    const dirPath = join(STORAGE_PATH, dirName);
     if (!existsSync(dirPath)) {
       return mkdir(dirPath);
     }
