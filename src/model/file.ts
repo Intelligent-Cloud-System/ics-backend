bf0a99ae3b48c41a6e30b8c2386e22222d9e52fb
Uryn Dmytro <dmytro@digia.co>
2022-02-20 13:18:48 +0200

    changed file model

--- src/model/file.ts
+++ src/model/file.ts
@@ -1,10 +1,24 @@
 import { NEW_ID } from 'src/shared/util/util';
+import path from 'path';
 
 export class File {
+
+  public name: string;
+  public basename: string;
+  public ext: string;
+  public dir: string;
+
   constructor(
-    public readonly filePath: string,
-    public readonly fileSize: number,
+    public readonly key: string,
     public readonly userId: number,
-    public readonly id: number = NEW_ID
-  ) {}
+    public size: string,
+    public readonly id: number = NEW_ID,
+    public readonly createdAt: Date = new Date(),
+  ) {
+    const parsed = path.parse(key);
+    this.name = parsed.name;
+    this.basename = parsed.base;
+    this.ext = parsed.ext;
+    this.dir = parsed.dir;
+  }
 }
