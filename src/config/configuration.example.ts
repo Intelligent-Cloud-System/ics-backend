d5236b51ba1196324dc65491a19421dfb31cf4d7
Maxim Govoruha <54854901+MaksGovor@users.noreply.github.com>
2022-05-06 13:05:01 +0300

    Update src/config/configuration.example.ts
    Co-authored-by: Uryn Dmytro <44302526+tedi4t@users.noreply.github.com>

--- src/config/configuration.example.ts
+++ src/config/configuration.example.ts
@@ -20,7 +20,7 @@ export default () => ({
   },
   swagger: {
     title: 'ICS',
-    description: 'Intelligent file_manager storage',
+    description: 'Intelligent file storage',
   },
   aws: {
     region: 'your-aws-region',
